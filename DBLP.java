f5e9ce6c7e8465df040be9afb6296aa18afa3a4c
sanchit97 <sanchit15083@iiitd.ac.in>
2016-11-05 11:25:08 +0530

    Main Function Added

--- DBLP.java
+++ DBLP.java
@@ -2,7 +2,11 @@ import java.util.*;
 import javax.swing.*;
 class DBLP
 {
-	
+	public static void main(String [] args)
+	{
+		System.out.println("");
+		System.out.println("");
+	}
 }
 class Parser
 {
